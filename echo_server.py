8bd4985d1e2abcd0370b49e935eae4c618c51004
dnhmd <adnnahmed@gmail.com>
2026-07-07 14:56:21 +0300

    chore: add echo server for tests

---
+++ echo_server.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+class EchoHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        response = {
+            "method": "GET",
+            "path": self.path,
+            "headers": dict(self.headers)
+        }
+        self.wfile.write(json.dumps(response).encode())
+
+    def log_message(self, format, *args):
+        print(f"[ECHO] {self.address_string()} - {args[0]}")
+
+if __name__ == "__main__":
+    server = HTTPServer(("127.0.0.1", 8081), EchoHandler)
+    print("Echo server running on http://127.0.0.1:8081")
+    server.serve_forever()
