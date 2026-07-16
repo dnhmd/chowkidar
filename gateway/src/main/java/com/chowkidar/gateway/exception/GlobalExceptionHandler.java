295ad47e72accb40d69fd25e8de7ec19012cc2cc
dnhmd <adnnahmed@gmail.com>
2026-07-16 11:14:26 +0300

    feat: add structured logging for unhandled exceptions

--- gateway/src/main/java/com/chowkidar/gateway/exception/GlobalExceptionHandler.java
+++ gateway/src/main/java/com/chowkidar/gateway/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.chowkidar.gateway.exception;
 
+import com.chowkidar.gateway.filter.ContextResolutionFilter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
@@ -16,9 +19,13 @@ import reactor.core.publisher.Mono;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
 @Component
 @Order(-1)
 public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     private final ObjectMapper objectMapper;
 
@@ -42,6 +49,11 @@ public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
         } else {
             httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
             message = "An unexpected error occurred.";
+
+            log.error("GlobalExceptionHandler | event=internal_server_error | Error: {}", ex.getMessage(),
+                    keyValue("path", exchange.getRequest().getPath()),
+                    keyValue("method", exchange.getRequest().getMethod().name()),
+                    keyValue("error", ex.getMessage()));
         }
 
         ErrorResponse errorResponse = new ErrorResponse(
