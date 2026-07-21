8ead39c9f476e279965266cf12af076286143d1b
dnhmd <adnnahmed@gmail.com>
2026-07-21 10:48:19 +0300

    feat: add root and pages

---
+++ portal/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Router from 'svelte-spa-router'
+  import Home from './pages/Home.svelte'
+  import Tenant from './pages/Tenant.svelte'
+
+  const routes = {
+    '/': Home,
+    '/tenant': Tenant,
+  }
+</script>
+
+<Router {routes} />
